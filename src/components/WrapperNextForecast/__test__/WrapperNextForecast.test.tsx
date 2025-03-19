a92230699983583043cb86781875b256481736f3
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-03-18 23:07:35 -0300

    test: create test of component Wrapper Next Forecast

---
+++ src/components/WrapperNextForecast/__test__/WrapperNextForecast.test.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+import { ThemeProvider } from "styled-components/native";
+import theme from "../../../general/theme";
+import WrapperNextForecast from "../index";
+
+jest.mock('@expo/vector-icons/Feather', () => 'FontAwesome5');
+
+describe("Component WrapperNextForecast", () => {
+    it("should render WrapperNextForecast correctly", () => {
+        render(
+            <ThemeProvider theme={theme}>
+                <WrapperNextForecast title="Next Forecast" />
+            </ThemeProvider>
+        );
+    });
+
+    it("should render title correctly", () => {
+        const { getByText } = render(
+            <ThemeProvider theme={theme}>
+                <WrapperNextForecast title="Next  Forecast" />
+            </ThemeProvider>
+        );
+
+        expect(getByText("Next Forecast")).toBeTruthy();
+    });
+
+    it("should render icon feather correctly", () => {
+        const { getByText, getByTestId } = render(
+            <ThemeProvider theme={theme}>
+                <WrapperNextForecast title="Next Forecast" />
+            </ThemeProvider>
+        );
+
+        expect(getByTestId("iconFeather")).toBeTruthy();
+    });
+});
--- src/components/WrapperNextForecast/index.tsx
+++ src/components/WrapperNextForecast/index.tsx
@@ -3,13 +3,16 @@ import Feather from '@expo/vector-icons/Feather';
 import { Wrapper, Day, Title, WrapperHeader } from "./styles";
 import theme from "../../general/theme";
 
+interface IWrapperNextForecast {
+    title: string;
+}
 
-export default function WrapperNextForecast() {
+export default function WrapperNextForecast(data: IWrapperNextForecast) {
     return (
         <Wrapper>
             <WrapperHeader>
-                <Title>Next Forecast</Title>
-                <Feather name="calendar" size={24} color={theme.colors.white} />
+                <Title>{data.title}</Title>
+                <Feather testID="iconFeather" name="calendar" size={24} color={theme.colors.white} />
             </WrapperHeader>
         </Wrapper>
     )
