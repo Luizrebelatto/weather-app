e2cde33f638748b1058a5731e2391e586cc7f5c5
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-03-27 19:31:16 -0300

    coverage

--- src/stores/forecastWeather/__test__/forecastWeatherStore.test.tsx
+++ src/stores/forecastWeather/__test__/forecastWeatherStore.test.tsx
@@ -20,34 +20,6 @@ describe('forecastWeather Store', () => {
     expect(result.current.forecastWeather).toEqual(mockForecastData);
   });
 
-  it('should update current weather multiple times', () => {
-    const { result } = renderHook(() => useForecastWeather());
-    
-    const anotherMockWeather: IForecastWeather = {
-      ...mockForecastData,
-      current: {
-        ...mockCurrentData.current,
-        temp_c: 30.5,
-        precip_in: 54, 
-        precip_mm: 22, 
-        pressure_in: 27.88, 
-        pressure_mb: 1047, 
-      },
-    };
-    
-    act(() => {
-      result.current.setForecastWeather?.(mockForecastData);
-    });
-    
-    expect(result.current.forecastWeather).toEqual(mockForecastData);
-    
-    act(() => {
-      result.current.setForecastWeather?.(anotherMockWeather);
-    });
-    
-    expect(result.current.forecastWeather).toEqual(anotherMockWeather);
-  });
-
   it('should handle setting weather to null', () => {
     const { result } = renderHook(() => useForecastWeather());
     
