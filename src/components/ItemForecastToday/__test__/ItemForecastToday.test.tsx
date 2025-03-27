68b9697357b277e9669d6c602ba7433f36346f45
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-03-27 07:59:28 -0300

    test: create tests to itemForecastToday

--- src/components/ItemForecastToday/__test__/ItemForecastToday.test.tsx
+++ src/components/ItemForecastToday/__test__/ItemForecastToday.test.tsx
@@ -5,15 +5,17 @@ import { ThemeProvider } from "styled-components/native";
 import theme from "general/theme";
 import ItemForecastToday from "../index";
 
-test('the component ItemForecastToday rendered', () => {
-    render(
-        <ThemeProvider theme={theme}>
-            <ItemForecastToday
-                description="Sunny" 
-                iconImage="//cdn.weatherapi.com/weather/64x64/day/116.png"
-                temperature="86.3 °F"
-                
-            />
-        </ThemeProvider>
-    )
+describe("ItemForecastToday",() => {
+    it('the component ItemForecastToday rendered', () => {
+        render(
+            <ThemeProvider theme={theme}>
+                <ItemForecastToday
+                    description="Sunny" 
+                    iconImage="//cdn.weatherapi.com/weather/64x64/day/116.png"
+                    temperature="86.3 °F"
+                    
+                />
+            </ThemeProvider>
+        )
+    })
 })
